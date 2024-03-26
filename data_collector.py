bcdce4f35d6b4b8b5c952ce2e297e8ef33d6468f
ant1234 <antonyvidetta1986@gmail.com>
2024-03-26 19:24:04 +1300

    collect older data from binance

--- data_collector.py
+++ data_collector.py
@@ -31,6 +31,54 @@ def collect_all(client: BinanceClient, exchange: str, symbol: str):
         #insert the data
 
     # most recent data
+    while True:
+
+        data = client.get_historical_data(symbol, start_time=int(most_recent_ts + 60000))
+
+        if data is None:
+            time.sleep(4) # pause incase an error occurs during the request
+            continue
+
+        if len(data) < 2:
+            break
+
+        data = data[:-1] 
+
+        if data[-1][0] > most_recent_ts:
+            most_recent_ts = data[-1][0]
+            
+        logger.info("%s %s: collected %s recent data from %s to %s ", 
+                        exchange, 
+                        symbol, 
+                        len(data),
+                        ms_to_dt(data[0][0]),
+                        ms_to_dt(data[-1][0]))
+            
+        time.sleep(1.1) # pause to overcome the rate limit
 
     # older data
+    while True:
+
+        data = client.get_historical_data(symbol, end_time=int(oldest_ts - 60000))
+
+        if data is None:
+            time.sleep(4) # pause incase an error occurs during the request
+            continue
+
+        if len(data) == 0:
+            logger.info("%s %s: stopped older data collection because no data was found before %s", 
+                        exchange, symbol, ms_to_dt(oldest_ts))
+            break
+
+        if data[0][0] < oldest_ts:
+            oldest_ts = data[0][0]
+            
+        logger.info("%s %s: collected %s older data from %s to %s ", 
+                        exchange, 
+                        symbol, 
+                        len(data),
+                        ms_to_dt(data[0][0]),
+                        ms_to_dt(data[-1][0]))
+            
+        time.sleep(1.1) # pause to overcome the rate limit
 
