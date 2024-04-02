cc606b3356e03a6b13127acec64fa3cdd97d018a
ant1234 <antonyvidetta1986@gmail.com>
2024-04-02 14:57:32 +1300

    add backtesting interface

--- main.py
+++ main.py
@@ -3,6 +3,9 @@
 import logging 
 from exchanges.binance import BinanceClient
 from data_collector import collect_all
+import backtester
+from utils import TF_EQUIV
+import datetime
 
 logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
@@ -21,7 +24,7 @@ logger.addHandler(stream_handler)
 logger.addHandler(file_handler)
 
 if __name__ == "__main__":
-    mode = input("choose the program mode (data / backtest / optimize)").lower()
+    mode = input("choose the program mode (data / backtest / optimize): ").lower()
 
     exchange = "binance"
 
@@ -35,3 +38,47 @@ if __name__ == "__main__":
 
     if mode == "data":
         collect_all(client, exchange, symbol)
+
+    elif mode == "backtest":
+
+        # strategy
+
+        available_strategies = ["obv"]
+
+        while True:
+            strategy = input(f"choose a strategy: ({', '.join(available_strategies)})").lower()
+            if strategy in available_strategies:
+                break
+
+        # from 
+
+        while True:
+            tf = input(f"choose a timeframe: ({', '.join(TF_EQUIV.keys())})").lower()
+            if tf in TF_EQUIV.keys():
+                break
+
+        while True:
+            from_time = input("backtest from (yyyy-mm-dd or press enter to use all data)").lower()
+            if from_time == "":
+                from_time = 0
+                break
+
+            try: 
+                from_time = int(datetime.datetime.strptime(from_time, "%Y-%m-%d").timestamp() * 1000)
+                break
+            except ValueError:
+                continue
+
+        while True:
+            to_time = input("backtest to (yyyy-mm-dd or press enter to use current time)").lower()
+            if to_time == "":
+                to_time = int(datetime.datetime.now().timestamp() * 1000)
+                break
+
+            try: 
+                to_time = int(datetime.datetime.strptime(to_time, "%Y-%m-%d").timestamp() * 1000)
+                break
+            except ValueError:
+                continue
+
+        backtester.run(exchange, symbol, strategy, tf, from_time, to_time)
